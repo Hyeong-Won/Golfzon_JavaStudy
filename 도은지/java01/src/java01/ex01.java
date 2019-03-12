a2890fac070c96bd4a078a280625f7a9a9dbcca3
ejdo <ejdo@paylettter.com>
2019-03-12 09:22:41 +0900

    ex01 commit

---
+++ 도은지/java01/src/java01/ex01.java
@@ -0,0 +1,34 @@
+package java01;
+
+import java.util.Arrays;
+
+public class ex01 {
+	public static void main(String[] args) {
+		
+		 int[] arr = new int[5];
+	      
+		 //배열에 랜덤으로 값 넣기
+	      for(int i=0; i<arr.length; i++) {
+	         arr[i] = (int)(Math.random()*100);
+	      }
+	      
+	      System.out.println("before :" + Arrays.toString(arr));
+	      
+	     //배열 복사하기
+	     int[] arr2 = Arrays.copyOf(arr, 5);
+	      
+	     //버블정렬
+	      for(int i=0; i<arr2.length-1; i++) {
+	         for(int j=0; j<arr2.length-1; j++) {
+	            if(arr2[j] > arr2[j+1]) {
+	               int tmp = arr2[j];
+	               arr2[j] = arr2[j+1];
+	               arr2[j+1] = tmp;
+	            }
+	         }
+	      }
+	      
+	      System.out.println("after :" + Arrays.toString(arr2));
+	      
+	}
+}
