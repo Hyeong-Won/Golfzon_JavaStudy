ad12c9c050db7e7ded2cc04f3dd8d4afd88ae24f
rialto76 <rialto76@payletter.com>
2019-03-12 19:30:07 +0900

    코드 추가

--- 이태영/chapter01/src/chapter01/chapter01.java
+++ 이태영/chapter01/src/chapter01/chapter01.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class chapter01 {
 	public static void main(String[] args) {
+		/*
 		int intTotalCnt = 0;
 		int intPosition  = 0;
 		
@@ -31,12 +32,37 @@ public class chapter01 {
 			intPosition = intPosition + 1;
 		}
 		
+		//배열 전체 값 출력
 		System.out.println( array_word);		
 		
+		//배열 요소 값 출력
 		for(int i=0;i<array_word.length;i++) {
 			System.out.println(  array_word[i]);
 		}
 		
 		scan.close();
+		*/
+		
+		//폴더에서 파일목록을 가져왔다 치고!
+		//그런데 String 배열 선언시 String[] arrFileList 와 String arrFileList[] 는 어떤 차이점이 있는지?
+		int intMatchedCnt = 0;
+		
+		String[] arrFileList = {"filelist1.txt","filelist2.csv","filelist3.txt","filelist4.csv","filelist5.txt"};
+		
+		for(int i=0;i < arrFileList.length;i++) {
+			
+			// split 함수의 인자는 정규식이다.  콤마의 경우 정규식 표현으로 무작위의 한글자를 의미
+			// 따라서 콤마는 \\ 를 붙여야 한다.
+			String[] arrFilename = arrFileList[i].split("\\.");
+			
+			// csv 확장자만 카운트하여 목록 출력하기
+			if(arrFilename[1].equalsIgnoreCase("csv")) {
+				intMatchedCnt = intMatchedCnt + 1;
+				System.out.println(arrFileList[i]);
+			}								
+		}
+		
+		System.out.printf("발견된 csv 파일 총 개수(%d)", intMatchedCnt);
+
 	}
 }
