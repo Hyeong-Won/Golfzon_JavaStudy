0a0bf1f8bc2d4bc7e9eddcceb81ff14b4074f541
magicnsword <magicnsword@gmail.com>
2019-03-20 21:35:36 +0900

    야구게임 클래스로 변경

---
+++ 이어진/NumberBaseBallGame/src/main/Main.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.util.Scanner;
+import baseBallGame.BaseBallGame;
+
+class Main 
+{ 
+	public static void main (String[] args)
+	{
+		Scanner s 				= new Scanner(System.in);
+		BaseBallGame baseBall	= new BaseBallGame();
+
+		// 스트라이크가 3이 될 때까지 check
+		while (baseBall.chkStrikeCnt()) 
+		{
+			System.out.println("=====================================================");
+			baseBall.addTryCnt();
+			System.out.println(baseBall.getTryCnt() + "번째 도전! 숫자를 입력해주세요. :D");
+
+			// 유저 숫자 입력
+			baseBall.inputUserNum(s);
+			
+			// 결과 체크
+			baseBall.checkResult();
+
+			// 결과 출력
+			baseBall.printResult();
+			
+			// 다음 체크를 위해 스트라이크, 볼 카운트 초기화
+			baseBall.resetCnt();
+		}
+		
+		s.close();
+	}
+}
