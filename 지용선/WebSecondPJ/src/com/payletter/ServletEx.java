5a6ee180ce669fa692e96dd61327df8227d457b3
yongsunJee <ysjee@payletter.com>
2019-05-14 18:01:36 +0900

    JSP+Servlet 2번째 과재

---
+++ 지용선/WebSecondPJ/src/com/payletter/ServletEx.java
@@ -0,0 +1,51 @@
+package com.payletter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.xml.internal.bind.CycleRecoverable.Context;
+
+/**
+ * Servlet implementation class ServletEx
+ */
+public class ServletEx extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletEx() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw = response.getWriter();
+		
+		pw.print(getServletConfig().getInitParameter("adminId").toString());
+		
+		pw.print(getServletContext().getInitParameter("imgDir").toString());
+		
+		getServletContext().setAttribute("img","text");
+		response.sendRedirect("/WebSecondPJ/test.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
